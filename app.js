a01bc503ffc6fe4003e962c78be3b1f05357f6b9
shokhruhmirzo <shaalimov923@gmail.com>
2023-12-07 06:50:06 -0800

    inital commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const elSelect = document.getElementById('select')
+const elBtn = document.getElementById('counter')
+const elText = document.getElementById('zikr')
+
+elSelect.addEventListener('change', function () {
+    elText.textContent = elSelect.value;
+    elBtn.disabled = false;
+    elCount.textContent = 0
+});
+
+elBtn.addEventListener('click', function () {
+    elCount.textContent = + elCount.textContent + 1
+});
+
+elResetBtn.addEventListener('click', function () {
+    elCount.textContent = 0;
+    elText.textContent = "Iltimos waytmoqchi bo'lgan zikrni tanlang :)";
+    elBtn.disabled = true;
+    elSelect.value = "Zikrni tanlang";
+})
+const elCount = document.getElementById('count')
+const elResetBtn = document.getElementById("reset")
