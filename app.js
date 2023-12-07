a0c34fdb8d64de6747e06b0d1681b87d44b17d09
shokhruhmirzo <shaalimov923@gmail.com>
2023-12-07 07:18:08 -0800

    salom

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const elSelect = document.getElementById('select')
 const elBtn = document.getElementById('counter')
 const elText = document.getElementById('zikr')
-let elCountch = document.getElementById('count')
+let elCount = document.getElementById('count')
 
 elSelect.addEventListener('change', function () {
     elText.textContent = elSelect.value;
     elBtn.disabled = false;
-    elCountch.textContent = 0
+    elCount.textContent = 0
 });
 
 elBtn.addEventListener('click', function () {
-    elCountch.textContent = + elCount.textContent + 1
+    elCount.textContent = + elCount.textContent +1
 });
 
 elResetBtn.addEventListener('click', function () {
-    elCountch.textContent = 0;
+    elCount.textContent = 0;
     elText.textContent = "Iltimos waytmoqchi bo'lgan zikrni tanlang :)";
     elBtn.disabled = true;
     elSelect.value = "Zikrni tanlang";
 })
-const elCount = document.getElementById('count')
-const elResetBtn = document.getElementById("reset")
